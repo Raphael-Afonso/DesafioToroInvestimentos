3cb47b726e5372deb1d147a447a779270dd7fb54
Raphael afonso <rapha.fernandes@live.com>
2021-07-08 21:42:02 -0300

    Criado modelo de Conta Corrente.

---
+++ ContaCorrente-Backend/Models/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿namespace ContaCorrente_Backend.Models
+{
+    public class ContaCorrente
+    {
+        public int ID { get; private set; }
+        public string Titular { get; private set; }
+        public string Inscricao { get; private set; }
+        public string Agencia { get; private set; }
+        public string Conta { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        public ContaCorrente(int iD, string titular, string inscricao, string agencia, string conta, decimal saldo)
+        {
+            ID = iD;
+            Titular = titular;
+            Inscricao = inscricao;
+            Agencia = agencia;
+            Conta = conta;
+            Saldo = saldo;
+        }
+    }
+}
