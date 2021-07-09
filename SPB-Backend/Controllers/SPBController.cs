b335f7b761da790d5fb9cd44728c7671341d0938
Raphael afonso <rapha.fernandes@live.com>
2021-07-08 21:03:33 -0300

    Adicionar arquivos de projeto.

---
+++ SPB-Backend/Controllers/SPBController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SPB_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SPBController : ControllerBase
+    {
+        private readonly ILogger<SPBController> _logger;
+
+        public SPBController(ILogger<SPBController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
