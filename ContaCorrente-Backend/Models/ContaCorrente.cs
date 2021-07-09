b3f77372a7885ff8ab1b66fa04dd034951d071cd
Raphael afonso <rapha.fernandes@live.com>
2021-07-08 22:47:01 -0300

    Modelado a classe para Conta Corrente.

--- ContaCorrente-Backend/Models/ContaCorrente.cs
+++ ContaCorrente-Backend/Models/ContaCorrente.cs
@@ -1,12 +1,18 @@
-﻿namespace ContaCorrente_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContaCorrente_Backend.Models
 {
     public class ContaCorrente
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; private set; }
         public string Titular { get; private set; }
         public string Inscricao { get; private set; }
         public string Agencia { get; private set; }
         public string Conta { get; private set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Saldo { get; private set; }
 
         public ContaCorrente(int iD, string titular, string inscricao, string agencia, string conta, decimal saldo)
@@ -17,6 +23,13 @@
             Agencia = agencia;
             Conta = conta;
             Saldo = saldo;
+        }
+        public ContaCorrente(string titular, string inscricao, string conta)
+        {
+            Agencia = "0001";
+            Titular = titular;
+            Inscricao = inscricao;
+            Conta = conta;
         }
     }
 }
