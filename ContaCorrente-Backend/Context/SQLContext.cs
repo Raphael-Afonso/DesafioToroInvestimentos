ebd22437a1d016c9ca64ded337db0168450b2a87
Raphael afonso <rapha.fernandes@live.com>
2021-07-08 22:47:38 -0300

    Definido o contexto com SQL <> Entity.

---
+++ ContaCorrente-Backend/Context/SQLContext.cs
@@ -0,0 +1,12 @@
+﻿using ContaCorrente_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContaCorrente_Backend.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options) { }
+
+        public DbSet<ContaCorrente> ContaCorrente { get; private set; }
+    }
+}
