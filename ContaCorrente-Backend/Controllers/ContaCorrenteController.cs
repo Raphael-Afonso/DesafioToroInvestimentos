75bc3347852bd01a0edbc275848df40d5a68624f
Raphael afonso <rapha.fernandes@live.com>
2021-07-08 21:35:32 -0300

    Criação do projeto para o serviço de ContaCorrente.

---
+++ ContaCorrente-Backend/Controllers/ContaCorrenteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContaCorrente_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContaCorrenteController : ControllerBase
+    {
+        private readonly ILogger<ContaCorrenteController> _logger;
+
+        public ContaCorrenteController(ILogger<ContaCorrenteController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
