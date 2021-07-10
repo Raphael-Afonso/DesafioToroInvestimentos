986c278c6c7ff651f4b89c04051037d72380cebb
Raphael afonso <rapha.fernandes@live.com>
2021-07-09 21:19:55 -0300

    Criação da classe pra comportar as regras de negócio impostas no processamento de movimentos do SPB e demais sobre contra corrente.

---
+++ ContaCorrente-Backend/Services/ContaCorrenteService.cs
@@ -0,0 +1,37 @@
+﻿using ContaCorrente_Backend.Context;
+using ContaCorrente_Backend.Models;
+using ContaCorrente_Backend.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ContaCorrente_Backend.Services
+{
+    public class ContaCorrenteService
+    {
+        private readonly ContaCorrenteRepository _repository;
+        public ContaCorrenteService(DbContextOptions<SQLContext> options)
+        {
+            _repository = new(options);
+        }
+
+        private ContaCorrente Depositar(MovimentoRequest movimento)
+        {
+            ContaCorrente ContraParte = _repository.BuscarPorAgenciaEConta(
+                movimento.ContraParte.Agencia, movimento.ContraParte.Conta);
+
+            if (ContraParte.Inscricao.Equals(movimento.Parte.CPF))
+                _repository.IncrementarSaldo(movimento.Valor, ContraParte);
+            else
+                throw new ApplicationException("CPF da parte é diferente da contra parte!");
+
+            return ContraParte;
+        }
+        public ContaCorrente ProcessarMovimento(MovimentoRequest movimento)
+        {
+            if (movimento.Evento.Equals("TRANSFER"))
+                return Depositar(movimento);
+            else
+                throw new ApplicationException("Evento não reconhecido!");
+        }
+    }
+}
