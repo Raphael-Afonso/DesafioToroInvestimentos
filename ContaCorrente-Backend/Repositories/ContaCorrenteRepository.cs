fe0a0f936a405cf05b4db9502afe39d887ecdac3
Raphael afonso <rapha.fernandes@live.com>
2021-07-09 21:20:22 -0300

    Adicionado métodos para adicionar saldo e buscar conta corrente.

--- ContaCorrente-Backend/Repositories/ContaCorrenteRepository.cs
+++ ContaCorrente-Backend/Repositories/ContaCorrenteRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ContaCorrente_Backend.Context;
 using ContaCorrente_Backend.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ContaCorrente_Backend.Repositories
 {
@@ -17,6 +18,24 @@ namespace ContaCorrente_Backend.Repositories
             _context.ContaCorrente.Add(contaCorrente);
             _context.SaveChanges();
             return contaCorrente;
+        }
+
+        public ContaCorrente BuscarPorAgenciaEConta(string agencia, string conta)
+        {
+            ContaCorrente ContaCorrente = _context.ContaCorrente
+                .Where(c => c.Agencia.Equals(agencia) && c.Conta.Equals(conta))
+                .FirstOrDefault();
+            if (ContaCorrente is not null)
+                return ContaCorrente;
+            else
+                throw new System.ApplicationException("Conta não localizada!");
+        }
+
+        public ContaCorrente IncrementarSaldo(decimal valorAReceber, ContaCorrente contaAReceber)
+        {
+            contaAReceber.Depositar(valorAReceber);
+            _context.SaveChanges();
+            return contaAReceber;
         }
     }
 }
