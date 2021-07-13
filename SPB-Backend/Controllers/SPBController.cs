2d8f6f7d61bec61c2b3a94a99df27c80d48af7b5
Raphael afonso <rapha.fernandes@live.com>
2021-07-12 21:06:17 -0300

    Criado a integração SPB <> Contacorrente.

--- SPB-Backend/Controllers/SPBController.cs
+++ SPB-Backend/Controllers/SPBController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SPB_Backend.Models;
+using SPB_Backend.Services;
 using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace SPB_Backend.Controllers
 {
@@ -10,16 +14,31 @@ namespace SPB_Backend.Controllers
     public class SPBController : ControllerBase
     {
         private readonly ILogger<SPBController> _logger;
+        private readonly SPBService _service;
 
-        public SPBController(ILogger<SPBController> logger)
+
+        public SPBController(ILogger<SPBController> logger, IConfiguration parametros)
         {
             _logger = logger;
+            _service = new(parametros);
         }
 
         [HttpPost]
         public async Task<ActionResult> PostSPB(SPB spb)
         {
-            return Ok();
+            try
+            {
+                await _service.EnviarMovimentoParaContaCorrente(spb);
+                return Ok();
+            }
+            catch (ApplicationException)
+            {
+                return StatusCode(StatusCodes.Status424FailedDependency);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- SPB-Backend/Services/SPBService.cs
+++ SPB-Backend/Services/SPBService.cs
@@ -1,7 +1,30 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using SPB_Backend.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
 namespace SPB_Backend.Services
 {
-    public static class SPBService
+    public class SPBService
     {
+        private readonly IConfiguration _parametros;
+        public SPBService(IConfiguration parametros)
+        {
+            _parametros = parametros;
+        }
+        public async Task EnviarMovimentoParaContaCorrente(SPB movimentoSPB)
+        {
+            using HttpClient HttpClient = new();
+            using HttpRequestMessage Request = new(HttpMethod.Put, new Uri($"http://{_parametros["ContaCorrenteHost"]}/contacorrente/processarMovimento"));
+            Request.Content = new StringContent(JsonSerializer.Serialize(movimentoSPB), Encoding.UTF8, "application/json");
+            
+            using HttpResponseMessage Response = await HttpClient.SendAsync(Request);
+
+            if (Response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new ApplicationException("Falha ao integrar com o serviço de conta corrente!");
+        }
     }
 }
